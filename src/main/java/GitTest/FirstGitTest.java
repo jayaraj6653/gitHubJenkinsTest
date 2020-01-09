978fb5d896d4129d9c2d56f386d6c0793c5bef0b
Jaya6653 <jraj261096@gmail.com>
2020-01-09 17:15:22 +0530

    add all

---
+++ src/main/java/GitTest/FirstGitTest.java
@@ -0,0 +1,30 @@
+package GitTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FirstGitTest {
+
+	@Test
+	public void loginTest() throws IOException {
+		// TODO Auto-generated method stub
+		WebDriver wd = new FirefoxDriver();
+		wd.get("https://www.bing.com");
+		WebElement input = wd.findElement(By.name("q"));
+		input.sendKeys("Chennai");
+		input.submit();
+		
+		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\SeleniumWorkspace\\GitHubTest\\screenshot\\screenshot\\git.png"));
+	}
+
+}
